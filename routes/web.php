5e4553d8bca8a15f9a9a84e4f32cd71663bb2d35
GustaBeholder <gustavo.hps222@gmail.com>
2020-09-07 18:57:03 -0300

    Teste com rotas get

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+   return view('welcome');
 });
+
+Route::get('contato/{id?}', function ($id = NULL) {
+   if($id == NULL) return 'Sem id definida, defina uma agora';
+   else return 'seu id é: '.$id;
+});
+
+
