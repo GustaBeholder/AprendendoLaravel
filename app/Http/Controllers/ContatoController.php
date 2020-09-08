4e34a6524b4d63e2e0c7d4b2e15cb42d43526dca
GustaBeholder <gustavo.hps222@gmail.com>
2020-09-07 21:20:30 -0300

    Models

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Contato;
+
 class ContatoController extends Controller
 {
     //
@@ -13,6 +15,11 @@ class ContatoController extends Controller
             (object)["nome"=>"Maria", "telefone"=>"64517782"],
             (object)["nome"=>"Jose", "telefone"=>"44741259"],
         ];
+
+        $contato = new Contato();
+        $con =  $contato->lista();
+        dd($con->nome);
+
         return view('contato.index', compact('contatos'));
     }
     
