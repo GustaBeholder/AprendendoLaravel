0f837207a99df7a2947ac17cafa95b2e56d09be5
GustaBeholder <gustavo.hps222@gmail.com>
2020-09-15 23:09:50 -0300

    Atuliazando e advionando

---
+++ resources/views/admin/cursos/adcionar.blade.php
@@ -0,0 +1,18 @@
+@extends('layout.tamplate')
+
+@section('titulo','Adcioanr Cursos')
+
+@section('conteudo')
+<div class="conteiner">
+    <h3 class="center"> Adcionar cursos</h3>
+    <div class="row">
+    <form class="" action="{{route('admin.cursos.salvar')}}" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        @include('admin.cursos._form')
+        <button class="btn deep-orange">Salvar</button>
+        </form>
+    </div>
+</div>
+
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,15 @@ Route::get('/', function(){
 
 Route::get('contato',['uses'=>'ContatoController@index']);
 
-Route::get('/admin/cursos',['as'=>'admim.cursos', 'uses'=>'Admin\CursoController@index']);
+Route::get('/admin/cursos',['as'=>'admin.cursos', 'uses'=>'Admin\CursoController@index']);
 
 Route::get('/admin/cursos/adcionar',['as'=>'admin.cursos.adcionar', 'uses'=>'Admin\CursoController@adcionar']);
 
 Route::post('/admin/cursos/salvar', ['as'=>'admin.cursos.salvar', 'uses'=>'Admin\CursoController@salvar']);
 
-Route::get('/admin/cursos/atualiar/{id}',['as'=>'admin.cursos.atualizar', 'uses'=>'Admin\CursoController@atualizar']);
+Route::get('/admin/cursos/atualiar/{id}',['as'=>'admin.cursos.editar', 'uses'=>'Admin\CursoController@editar']);
 
-Route::put('/admin/cursos/editar/{id}', ['as'=> 'admin.cursos.atualiar', 'uses'=>'Admin\CursoController@editar']);
+Route::post('/admin/cursos/editar/{id}', ['as'=> 'admin.cursos.atualizar', 'uses'=>'Admin\CursoController@atualizar']);
 
 Route::get('admin/cursos/deletar/{id}', ['as'=>'admin.cursos.deletar', 'uses'=>'Admin\CursoController@deletar']);
 
